c1e8918ba4aeb1c9920ccd431d06b28dd1f77387
Taylor C. Richberger <taywee@gmx.com>
2020-06-05 00:49:35 -0600

    make parsers generic to make some things easier

--- src/bin/asciimusic.rs
+++ src/bin/asciimusic.rs
@@ -1,10 +1,10 @@
 extern crate asciimusic;
 
-use asciimusic::parser::Error;
 use asciimusic::parser::parse_song;
 use asciimusic::Song;
 use std::io;
 use std::io::prelude::*;
+use nom::IResult;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = r#"
@@ -12,7 +12,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     1 0,0/1,0.005/0.75,0.01/0,-0.01 1 512 UA QA AA QA AA MA UB TB AB EB AB
     "#;
 
-    let (_, song) = parse_song(input).unwrap();
+    let result: IResult<&str, Song> = parse_song(input);
+    let (_, song) = result?;
     println!("{:#?}", song);
     Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,34 +1,55 @@
-mod error;
-pub use error::Error;
+//mod error;
+//pub use error::Error;
 
 use nom::{
   IResult,
   // see the "streaming/complete" paragraph lower for an explanation of these submodules
   bytes::complete::{take_while, take_while1},
   character::complete::digit1,
+  error::{ParseError, ErrorKind}, 
+  AsBytes, InputLength, Slice, AsChar,
+  InputTakeAtPosition,
 }; 
+
+use std::ops::RangeFrom;
 
 use crate::song::Song;
 
-fn parse_digits(input: &str) -> IResult<&str, u64, Error<&str>> {
+fn parse_digits<T, E>(input: T) -> IResult<T, u64, E>
+where
+    T: AsBytes + InputLength + Slice<RangeFrom<usize>> + InputTakeAtPosition,
+    <T as InputTakeAtPosition>::Item: AsChar,
+    E: ParseError<T>,
+{
     let (input, sample_rate) = digit1(input)?;
-    let sample_rate = sample_rate.parse().map_err(|e| nom::Err::Failure(Error::from(e)))?;
+    let sample_rate = match std::str::from_utf8(sample_rate.as_bytes()).unwrap().parse() {
+        Ok(rate) => rate,
+        // TODO: use _e for context
+        Err(_e) => return Err(nom::Err::Failure(E::from_error_kind(input, ErrorKind::Digit))),
+    };
     Ok((input, sample_rate))
 }
 
-fn is_space(c: char) -> bool {
+fn is_space<T: AsChar>(c: T) -> bool {
+    let c = c.as_char();
     c == ' ' || c == '\t'
 }
 
-fn is_newline(c: char) -> bool {
+fn is_newline<T: AsChar>(c: T) -> bool {
+    let c = c.as_char();
     c == '\r' || c == '\n'
 }
 
-fn is_whitespace(c: char) -> bool {
+fn is_whitespace<T: AsChar + Copy>(c: T) -> bool {
     is_space(c) || is_newline(c)
 }
 
-pub fn parse_song(input: &str) -> IResult<&str, Song, Error<&str>> {
+pub fn parse_song<T, E>(input: T) -> IResult<T, Song, E> 
+where 
+    T: AsBytes + InputLength + Slice<RangeFrom<usize>> + InputTakeAtPosition,
+    <T as InputTakeAtPosition>::Item: AsChar + Copy,
+    E: ParseError<T>,
+{
     let (input, _) = take_while(is_whitespace)(input)?;
     let (input, ticks_per_second) = parse_digits(input)?;
     let (input, _) = take_while1(is_space)(input)?;
